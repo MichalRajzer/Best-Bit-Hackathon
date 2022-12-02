69ad95019acc751c1fd74414aabf045cb3ecd6cc
Michal Rajzer <michal.rajzer03@gmail.com>
2022-12-02 20:49:24 +0100

    Created basic game loop logic

--- game/__init__.py
+++ game/__init__.py
@@ -8,17 +8,32 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
     clock = pygame.time.Clock()
     running = True
-    inMenu = True  # This is the variable that will be used to determine if the game is in the menu or not
-    inGame = False  # This is the variable that will be used to determine if the game is in the game loop or not
+    inMenu = True    # This is the variable that will be used to determine if the game is in the menu or not
+    inGame = False   # This is the variable that will be used to determine if the game is in the game loop or not
     inPause = False  # This is the variable that will be used to determine if the game is in the pause menu or not
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        screen.fill((70, 0, 70))
+        while inMenu:
+            # Path: game\mainmenu.py
+            # This is where the menu code will go
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        inMenu = False
+                        running = False
+                    elif event.key == pygame.K_RETURN:
+                        inMenu = False
+                        running = False
+                    if event.key == pygame.K_SPACE:
+                        inMenu = False
+                        inGame = True
+            screen.fill((70, 0, 70))
+            pygame.display.flip()
+            clock.tick(60)
+        while inGame:
+            pass
 
-        # Flip the display
-        pygame.display.flip()
-        clock.tick(60)
+            # Flip the display
     pygame.quit()
     sys.exit()
