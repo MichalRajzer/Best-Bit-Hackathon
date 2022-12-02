afe3b3905e2fdc3087d1017ee689ecb2a5b8ed8b
Michal Rajzer <michal.rajzer03@gmail.com>
2022-12-02 20:27:45 +0100

    Basic gameloop setup

--- game/__init__.py
+++ game/__init__.py
@@ -1,6 +1,24 @@
-# TODO MAKE MAIN MENU CLASS
-# TODO MAKE A GAME LOOP
-# TODO MAKE A OPTIONS MENU - JS
-# TODO MAKE SAVES
-# TODO MAKE ENEMIES
-# TODO MAKE SOUNDS 
+import pygame
+import sys
+
+if __name__ == "__main__":
+    # Path: game\__init__.py
+    pygame.init()
+    pygame.display.set_caption("My Game")
+    screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+    clock = pygame.time.Clock()
+    running = True
+    inMenu = True  # This is the variable that will be used to determine if the game is in the menu or not
+    inGame = False  # This is the variable that will be used to determine if the game is in the game loop or not
+    inPause = False  # This is the variable that will be used to determine if the game is in the pause menu or not
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((70, 0, 70))
+
+        # Flip the display
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
+    sys.exit()
